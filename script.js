7635dd6369ec9496168cdb9ebc30b196b3dbfac2
MakottoSan <diogomakotto@gmail.com>
2022-03-11 10:15:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var nome = "Diogo"
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 7
+var notaTerceiroBimestre = 4
+var notaQuartoBimestre = 2
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+
+console.log ("Boas vindas " + nome)
+console.log("Sua nota final foi: " + notaFixada)
+if (notaFinal >=7)
+  {console.log("Você foi aprovado, parabéns!!! ")}
+else (console.log("Você foi reprovado "))
+
+// Ótima aula
+
+// Revisão
+  // variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
